149288a2d6151f969ab2d5f9cbc5533fb034c749
Fábio Rodrigues <fabur13@gmail.com>
2021-05-12 16:20:03 -0300

    Updates

---
+++ precache-manifest.c7ed5d64ce73f7bd0aecff1aa3644133.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979f9d0a7e8e236104b019441e9a32a8",
+    "url": "/Desafio-10-GoRestaurant-Web/index.html"
+  },
+  {
+    "revision": "02d951fca539386db0ed",
+    "url": "/Desafio-10-GoRestaurant-Web/static/js/2.994f917c.chunk.js"
+  },
+  {
+    "revision": "5f8795b8f28c7d1a90992cb44145be0f",
+    "url": "/Desafio-10-GoRestaurant-Web/static/js/2.994f917c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30aa2a14d95e24839df5",
+    "url": "/Desafio-10-GoRestaurant-Web/static/js/main.994e66fb.chunk.js"
+  },
+  {
+    "revision": "967bc152e5550655854e",
+    "url": "/Desafio-10-GoRestaurant-Web/static/js/runtime-main.727befc9.js"
+  },
+  {
+    "revision": "633a71f08c54a1d5e0a780ef132408b2",
+    "url": "/Desafio-10-GoRestaurant-Web/static/media/logo.633a71f0.svg"
+  }
+]);
